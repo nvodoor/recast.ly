b8ec20f2044897b6d92a8dd7fa4b81c6f64c782a
nvodoor <nvodoor@gmail.com>
2017-02-22 21:57:50 -0800

    Commit Before Click Test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
-var App = (props) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={props.videos[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={props.videos}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: window.exampleVideoData,
+      currentvideo: null
+    };
+  }
+
+  onVideoClick(video) {
+    console.log(video);
+    this.setState({currentvideo: video});
+  }
+
+  render() {
+    return (
+    <div>
+      <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.videos[0]}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList Clicker={this.onVideoClick.bind(this)} videos={this.state.videos}/>
+        </div>
+    </div>);
+  }                                     
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
