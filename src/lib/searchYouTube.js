1f0fd4a5f397ec1bbfc3c3edd3733dc31e7bba64
nvodoor <nvodoor@gmail.com>
2017-02-23 19:24:01 -0800

    All Tests Passed. All Learn Prompts Passed.

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -3,7 +3,7 @@ var searchYouTube = (options, callback) => {
   $.ajax({
     url: 'https://www.googleapis.com/youtube/v3/search',
     type: 'GET',
-    data: {key: options.key, q: options.query, maxResults: options.max, part: 'snippet', order: 'date'},
+    data: {key: options.key, q: options.query, maxResults: options.max, part: 'snippet', order: 'date', videoEmbeddable: 'true', type: 'video'},
     contentType: 'application/json',
     success: function(data) {
       callback(data.items);
