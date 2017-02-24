fd1711c51289b3dd8add80b52c5fd2977705eb9e
nvodoor <nvodoor@gmail.com>
2017-02-23 19:56:19 -0800

    Added autoplay functionality.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 var Search = (props) => (
   <div className="search-bar form-inline">
     <input onChange={(event) => props.inputTextEvent(event.target.value)} className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
+    <button className="btn hidden-sm-down" onClick={() => autoplay ? autoplay = 0 : autoplay = 1}>
       <span className="glyphicon glyphicon-search"></span>
     </button>
   </div> 
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,7 +3,7 @@ var baseURL = 'https://www.youtube.com/embed/';
 var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={baseURL + props.video.id.videoId} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={baseURL + props.video.id.videoId + '?autoplay=' + autoplay} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
       <h3>{props.video.snippet.title}</h3>
