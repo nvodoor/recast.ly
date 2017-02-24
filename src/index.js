4f951b7f03f9c99b55cbf419772db1127923b50b
nvodoor <nvodoor@gmail.com>
2017-02-23 20:19:51 -0800

    Add autoplay function.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // TODO: Render the `App` component to the DOM
 
 //searchYouTube({key: 'AIzaSyCbs-YSZJ_Ya37LFtEhfW7eayDyrJFJIMc', query: 'dogs', maxResults: 5}, (data) => {data; });
-window.autoplay = 1;
+
 ReactDOM.render(<App searchYouTube={window.searchYouTube}/>, document.getElementById('app'));
 
 //test
